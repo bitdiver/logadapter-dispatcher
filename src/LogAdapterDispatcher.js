2c8f979554c06969d9bf47bd9d91248c4991a52e
Torsten Link <torstenlink@gmx.de>
2019-02-22 15:35:30 +0100

    fix(level): add missing methods

--- src/LogAdapterDispatcher.js
+++ src/LogAdapterDispatcher.js
@@ -17,6 +17,37 @@ export default class LogAdapterDispatcher {
       return Promise.all(promisses)
     }
   }
+  /**
+   * sets a new log level
+   * @param level {number/string} The logLevel as number or String value
+   */
+  set level(level) {
+    if (this.logAdapterList !== undefined && this.logAdapterList.length > 0) {
+      for (const logger of this.logAdapterList) {
+        logger.level = level
+      }
+    }
+  }
+  /**
+   * Returns the logLevel as a string
+   * @return level {string} The logLevel
+   */
+  get level() {
+    if (this.logAdapterList !== undefined && this.logAdapterList.length > 0) {
+      return this.logAdapterList[0].level
+    }
+  }
+
+  /**
+   * Returns the logLevel as a number
+   *
+   * @return level {string} The logLevel
+   */
+  get levelNumber() {
+    if (this.logAdapterList !== undefined && this.logAdapterList.length > 0) {
+      return this.logAdapterList[0].levelNumber
+    }
+  }
 
   reset() {
     if (this.logAdapterList !== undefined && this.logAdapterList.length > 0) {
