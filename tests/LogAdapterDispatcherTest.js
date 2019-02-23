0755f863f023ea10ddb93fb7c8e17fd3e6ce09ee
Torsten Link <torstenlink@gmx.de>
2019-02-23 21:46:57 +0100

    tests(logger): add further tests

--- tests/LogAdapterDispatcherTest.js
+++ tests/LogAdapterDispatcherTest.js
@@ -10,6 +10,33 @@ test('create a new LogAdapterDispatcher', async done => {
 /*
  * Test that the reset will be executed for all the logger
  */
+test('Logadapter set loglevel', async done => {
+  const logger1 = new DemoAdapter({ name: 'logger1' })
+  const logger2 = new DemoAdapter({ name: 'logger2' })
+  const logAdapter = new LogAdapterDispatcher({
+    logAdapterList: [logger1, logger2],
+  })
+
+  logAdapter.level = 3
+  expect(logAdapter.level).toEqual(3)
+  expect(logAdapter.levelNumber).toEqual(3)
+
+  expect(logger1.level).toEqual(3)
+  expect(logger2.level).toEqual(3)
+
+  logAdapter.level = 1
+
+  expect(logAdapter.level).toEqual(1)
+  expect(logAdapter.levelNumber).toEqual(1)
+  expect(logger1.level).toEqual(1)
+  expect(logger2.level).toEqual(1)
+
+  done()
+})
+
+/*
+ * Test that the reset will be executed for all the logger
+ */
 test('Logadapter reset', async done => {
   const logger1 = new DemoAdapter({ name: 'logger1' })
   const logger2 = new DemoAdapter({ name: 'logger2' })
@@ -70,6 +97,7 @@ class DemoAdapter {
   constructor(opts = {}) {
     this.name = opts.name
     this.messages = []
+    this.level = 0
   }
   async log(logMessage) {
     if (logMessage === 'c') {
@@ -77,6 +105,12 @@ class DemoAdapter {
     }
     this.messages.push(logMessage)
   }
+
+  // dummy implementation.
+  get levelNumber() {
+    return this.level
+  }
+
   reset() {
     this.messages = []
   }
